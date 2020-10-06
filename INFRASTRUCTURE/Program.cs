db160d1a0c8eda515224c08f88b6870c5d591be8
Steve Sutton <steven.p.sutton@gmail.com>
2020-10-06 13:07:54 +0100

    Initial creation

---
+++ INFRASTRUCTURE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace INFRASTRUCTURE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
