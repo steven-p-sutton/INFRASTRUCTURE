852c4c5c2e95083703dd04ce3f5688ed410b6e02
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-22 12:50:15 +0000

    try/catch around all file data file types

--- FILE-DataFormats/Program.cs
+++ FILE-DataFormats/Program.cs
@@ -36,12 +36,12 @@ namespace Conductus.FILE.ConsoleApp
             // Directory.GetCurrentDirectory()
             string root = "C:\\Users\\steve\\Documents\\Visual Studio Work\\INFRASTRUCTURE\\FILE-DataFormats\\";
 
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine("1. STREAM Files");
+            Console.WriteLine("--------------------------------------------------------------------");
+
             try
             {
-                Console.WriteLine("--------------------------------------------------------------------");
-                Console.WriteLine("1. STREAM Files");
-                Console.WriteLine("--------------------------------------------------------------------");
-
                 string streamFileName = "Stream.txt";
                 DataFileStream fs = new DataFileStream();
 
@@ -61,45 +61,74 @@ namespace Conductus.FILE.ConsoleApp
             Console.WriteLine("2. TEXT Files");
             Console.WriteLine("--------------------------------------------------------------------");
 
-            string textFileName = "Text.txt";
-            DataFileText ft = new DataFileText();
+            try
+            {
+                string textFileName = "Text.txt";
+                DataFileText ft = new DataFileText();
 
-            //ft.Create(root + textFileName);
-            //ft.Write(root + textFileName, m_widget1);
-            //m_widget2 = ft.Read(root + textFileName);
+                //ft.Create(root + textFileName);
+                //ft.Write(root + textFileName, m_widget1);
+                //m_widget2 = ft.Read(root + textFileName);
+            }
+            catch (NotImplementedException e)
+            {
+                // do nothing & continue
+            }
 
             Console.WriteLine("--------------------------------------------------------------------");
             Console.WriteLine("3. CSV Files");
             Console.WriteLine("--------------------------------------------------------------------");
 
-            string csvFileName = "Csv.csv";
-            DataFileCsv fc = new DataFileCsv();
+            try
+            {
+                string csvFileName = "Csv.csv";
+                DataFileCsv fc = new DataFileCsv();
 
-            //fc.Create(root + csvFileName);
-            //fc.Write(root + csvFileName, m_widget1);
-            //m_widget2 = fc.Read(root + csvFileName);
+                //fc.Create(root + csvFileName);
+                //fc.Write(root + csvFileName, m_widget1);
+                //m_widget2 = fc.Read(root + csvFileName);
+            }
+            catch (NotImplementedException e)
+            {
+                // do nothing & continue
+            }
 
             Console.WriteLine("-------------------------------------------------------------------");
             Console.WriteLine("4. JSON Files");
             Console.WriteLine("-------------------------------------------------------------------");
+            
+            try
+            {
+                string jsonFileName = "Json.json";
+                DataFileJson fj = new DataFileJson();
 
-            string jsonFileName = "Json.json";
-            DataFileJson fj = new DataFileJson();
 
-            //fj.Create(root + jsonFileName);
-            //fj.Write(root + jsonFileName, m_widget1);
-            //m_widget2 = fj.Read(root + jsonFileName);
+                //fj.Create(root + jsonFileName);
+                //fj.Write(root + jsonFileName, m_widget1);
+                //m_widget2 = fj.Read(root + jsonFileName);
+            }
+            catch (NotImplementedException e)
+            {
+                // do nothing & continue
+            }
 
             Console.WriteLine("-------------------------------------------------------------------");
             Console.WriteLine("4. XML Files");
             Console.WriteLine("-------------------------------------------------------------------");
 
-            string xmlFileName = "XML.xml";
-            DataFileXml fx = new DataFileXml();
+            try
+            {
+                string xmlFileName = "XML.xml";
+                DataFileXml fx = new DataFileXml();
 
-            //fx.Create(root + xmlFileName);
-            //fx.Write(root + xmlFileName, m_widget1);
-            //m_widget2 = fx.Read(root + xmlFileName);
+                //fx.Create(root + xmlFileName);
+                //fx.Write(root + xmlFileName, m_widget1);
+                //m_widget2 = fx.Read(root + xmlFileName);
+            }
+            catch (NotImplementedException e)
+            {
+                // do nothing & continue
+            }
 
             Console.WriteLine("-------------------------------------------------------------------");
             Console.WriteLine("99. ALL DONE");
