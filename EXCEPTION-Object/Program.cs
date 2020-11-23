0c1834720004aa98b9ad043a6bbe95f06a519702
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-23 08:32:55 +0000

    Added UTILITY project to hold Heading strings etc. Refactored EXCEPTION-Object project as a .NET Core app type to be compatible with others here

---
+++ EXCEPTION-Object/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conductus.Widget.Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
