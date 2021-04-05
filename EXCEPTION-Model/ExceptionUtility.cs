781369bbe66f10c41372b2552a5ce5f9fe45d3ab
steven-p-sutton <steven.p.sutton@gmail.com>
2021-04-05 10:00:55 +0100

    better

--- EXCEPTION-Model/ExceptionUtility.cs
+++ EXCEPTION-Model/ExceptionUtility.cs
@@ -2,18 +2,14 @@
 {
     static public string Display(System.Exception e, string Name)
     {
-        string s = ""; ;  
-            s = s + s.CRLF
-            + s.H3 
-            + s.Pad 
-            + "Name:" + Name 
-            + s.Pad 
-            + s.H3 
-            + s.CRLF
-            + "       Message:" 
-            + e.Message 
-            + s.CRLF;
-        //s = s + " InnerExeption:" + e.InnerException.ToString() + Environment.NewLine;
+        string s = string.Empty;  
+        s += s.CRLF()
+        + s.H3() + s.Pad() + "Name:" + Name + s.Pad() + s.H3() 
+        + s.CRLF()
+        + s.Pad() +  "Message:" + e.Message 
+        //+ s.CRLF()
+        //+ s.Pad() + "InnerExeption:" + e.InnerException.ToString() 
+        + s.CRLF();
         return s;
     }
 }
--- EXTENSION-Model/StringExtensions.cs
+++ EXTENSION-Model/StringExtensions.cs
@@ -2,39 +2,43 @@
 
 public static class StringExtensions
 {
-    public static string H1 (this string s, string value)
+    private const int _nMin = 1;
+    private const int _nMax = 6;
+    public static string H1 (this string s, int n = _nMax)
     {
-        return new String('*', 6);
+        return new String('*', n);
     }
-    public static string H2(this string s, string value)
+    public static string H2(this string s, int n = _nMax)
     {
-        string h2 = new String('=', 6);
-        return h2;
+        return new String('=', n);
     }
-    public static string H3(this string s, string value)
+    public static string H3(this string s, int n = _nMax)
     {
-        string h3 = new String('-', 6);
-        return h3;
+        return new String('-', n);
     }
-    public static string H4(this string s, string value)
+    public static string H4(this string s, int n = _nMax)
     {
-        return new String('.', 6);
+        return new String('.', n);
     }
-    public static string H5(this string s, string value)
+    public static string H5(this string s, int n = _nMax)
     {
-        return new String(' ', 6);
+        return new String(' ', n);
     }
-    public static string Pad(this string s, string value)
+    public static string Pad(this string s, int n = _nMax)
     {
-        return new String(' ', 6);
+        return new String(' ', n);
     }
-    public static string Div(this string s, string value)
+    public static string Div(this string s, int n = _nMax)
     {
-        return new String('-', 6);
+        return new String('-', n);
     }
-    public static string CRLF(this string s, string value)
+    public static string CRLF(this string s, int n = _nMin)
     {
-        string crlf = Environment.NewLine;
-        return crlf;
+        string tmp = string.Empty;
+
+        for (int i = 0; i < n; i++)
+            tmp += Environment.NewLine;
+
+        return tmp;
     }
 }
