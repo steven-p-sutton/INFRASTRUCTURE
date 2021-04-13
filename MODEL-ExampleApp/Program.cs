150d58a5ba438a7712c9d6ac176d33c9c8fe4667
steven-p-sutton <steven.p.sutton@gmail.com>
2021-04-13 22:10:55 +0100

    update

---
+++ MODEL-ExampleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MODEL_ExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
