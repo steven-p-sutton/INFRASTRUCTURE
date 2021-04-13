1587ef52670f3a1fa5129e0aba2321e1a9a7f1fb
steven-p-sutton <steven.p.sutton@gmail.com>
2021-04-13 15:38:33 +0100

    update

---
+++ MODEL-Example/EExample.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+public class EExample : IExample
+{
+    [JsonProperty("str")]
+    public string Str 
+    {
+        get
+        {
+            throw new ExampleNotImplentedException(); 
+        }
+            
+        set
+        {
+            throw new ExampleNotImplentedException(); 
+        }
+    }
+    [JsonProperty("int")]
+    public int Int 
+    {
+        get
+        {
+            throw new ExampleNotImplentedException();
+        }
+        set
+        {
+            throw new ExampleNotImplentedException();
+        }
+    }
+    [JsonProperty("list")]
+    public List<string> Repository 
+    {
+        get
+        {
+            throw new ExampleNotImplentedException();
+        }
+        set
+        {
+            throw new ExampleNotImplentedException();
+        } 
+    }
+    public int Add(string item)
+    {
+        throw new ExampleNotImplentedException();
+    }
+    public string Remove(int idx)
+    {
+        throw new ExampleNotImplentedException();
+    }
+    public int Find(string item)
+    {
+        throw new ExampleNotImplentedException();
+    }
+}
